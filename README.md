5bd829458dd0f3a0d9ce302887a2306797c2a9a5
Yeadee <yeadihasanpranto@gmail.com>
2023-02-12 09:00:21 +0000

    Update index.py
    updates

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">
+  TOFFEE
+</h1>
+<h2 align="center">
+<p align=center> Let's watch Toffee!</p>
+</h2>
+
+### <p align=center>How to Use</p>
+Auto updates. Just Enjoy!
---
+++ api/index.py
@@ -0,0 +1,59 @@
+import requests, re, json, base64, os
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+def data_decrypt(key, ciphertext):
+    cipher = AES.new(key, AES.MODE_ECB)
+    ciphertext = base64.b64decode(ciphertext)
+    padded_plaintext = cipher.decrypt(ciphertext)
+    plaintext = unpad(padded_plaintext, AES.block_size)
+    return plaintext.decode()
+
+def toffupdate():
+    inf = os.environ['TOFFEE_HEADER1']
+    mp_encoder = MultipartEncoder(fields={'data': inf})
+    fheader = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'Content-Type': mp_encoder.content_type}
+    furl = os.environ['TOFFEE_URL1']
+    res = requests.post(furl,data=mp_encoder,headers=fheader,proxies=proxy)
+    secret_key=os.environ['TOFFEE_SECRET_KEY']
+    try:
+        dbdata = json.loads(data_decrypt(secret_key.encode(),res.text))["response"]["dbVersionV2"]
+    except:
+        data={"message":"proxy not working"}
+        with open("toffee_channel_data.json","w") as w:
+            json.dump(data,w,indent=2)
+        return "proxy not working."
+    version = "1793"
+    for item in dbdata:
+        if item["api_name"]=="getUgcAppHomePageContentTofeeV2":
+            version = item["db_version"]
+            pass
+    url2=os.environ['TOFFEE_URL2']
+    api=f"{url2}{version}"
+    headers={"client-api-header":os.environ['TOFFEE_HEADER2']}
+    req=requests.get(url=api,headers=headers,proxies=proxy)
+    categories=json.loads(data_decrypt(secret_key.encode(),req.text))["response"]["categories"]
+    #with open("toffee_raw_data.json","w") as w:
+    #    json.dump(categories,w,indent=2)
+    all_data=[]
+    for category in categories:
+        for channel in category['channels']:
+            data = {}
+            data['id'],data['name'],data['logo'],data['url'],data['cookie'] = channel['id'],channel['program_name'],channel['channel_logo'],channel['plain_hls_url'],channel['sign_cookie']
+            if "streamer" in channel['plain_hls_url']:
+                data['url'] = channel['plain_hls_url_for_url_type']
+            all_data.append(data)
+    fulldata = {} 
+    fulldata['info'] = all_data
+    with open("toffee_channel_data.json","w") as w:
+        json.dump(fulldata,w,indent=2)
+        print(f"\n\n\t\t\t\t\t\tToffee Data loaded! version {version}")
+    return None
+
+proxy=json.loads(os.environ['BDPROXY'])
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
+toffupdate()
+
